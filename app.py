da0a8be7bf84091f782c5fc98d698934809e85bd
JackieW001 <jwoo@stuy.edu>
2017-11-09 18:50:57 -0500

    made root() and put coolios styling

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 import urllib2
 import json
 
 app = Flask(__name__)
 
-#https://api.nasa.gov/planetary/apod?api_key=1HbGn9RGplzF9EfsWk4vPWh6T24qOzYFbSbDu0Gi
-
 @app.route("/")
 def root():
     apod = urllib2.urlopen('https://api.nasa.gov/planetary/apod?api_key=1HbGn9RGplzF9EfsWk4vPWh6T24qOzYFbSbDu0Gi')
-    json = urllib.read(apod)
-    dict = json.load(json)
-    return render_template('apod.html', pic=dict['url'], explain=dict['explanation'])
+    apod_dict = json.loads(apod.read())
+    return render_template('apod.html', apod_img=apod_dict['url'], apod_title=apod_dict["title"], apod_desc=apod_dict["explanation"])
 
 if __name__ == '__main__':
   app.debug = True
